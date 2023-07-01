1c59829e8984309ef37e5c44eb499bca3a855c39
Renata Veras Venturim <129551549+RenataVerasVenturim@users.noreply.github.com>
2023-07-01 12:15:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* JS Document */
+
+/*Objetivo: Criar calculadora*/
+
+/*Declaração das variáveis*/
+
+/*Entrada de dados*/
+function calcular_resultado(){
+/*Saída de dados*/
+window.alert('Em construção');
+}
